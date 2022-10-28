413dde08f15801371392ea9c947654a6bb9bcc0e
genilson <geniilsonfernandes@gmail.com>
2022-10-28 15:27:09 -0300

    feat: create PercentCard component

---
+++ src/components/PercentCard/index.tsx
@@ -0,0 +1,31 @@
+import * as S from "./styles";
+import { Feather } from "@expo/vector-icons";
+import { useTheme } from "styled-components/native";
+
+export type PercentCardProps = {
+  percent?: number;
+  limit?: number;
+};
+
+export const PercentCard = ({ percent = 0, limit = 75 }: PercentCardProps) => {
+  const theme = useTheme();
+  const color = percent > limit ? "WARNING" : "NORMAL";
+
+  return (
+    <S.Wrapper backgroundColor={color}>
+      <S.Title>{percent}%</S.Title>
+      <S.SubTitle>das refeições dentro da dieta</S.SubTitle>
+      <S.Icon>
+        <Feather
+          name="arrow-up-right"
+          size={24}
+          color={
+            color === "NORMAL"
+              ? theme.COLORS.BRAND.GREEN.DARK
+              : theme.COLORS.BRAND.RED.DARK
+          }
+        />
+      </S.Icon>
+    </S.Wrapper>
+  );
+};
---
+++ src/components/PercentCard/styles.ts
@@ -0,0 +1,42 @@
+import { TouchableOpacity } from "react-native";
+import styled, { css } from "styled-components/native";
+
+type WrapperProps = {
+  backgroundColor?: "NORMAL" | "WARNING";
+};
+
+export const Wrapper = styled(TouchableOpacity)<WrapperProps>`
+  ${({ theme, backgroundColor }) => css`
+    background: ${backgroundColor === "NORMAL"
+      ? theme.COLORS.BRAND.GREEN.LIGHT
+      : theme.COLORS.BRAND.RED.LIGHT};
+    justify-content: center;
+    width: 100%;
+    padding: 20px;
+    border-radius: ${theme.RADIUS}px;
+    align-items: center;
+  `}
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XX}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-weight: 600;
+    color: ${theme.COLORS.BASE.GRAY_100};
+  `}
+`;
+export const SubTitle = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-weight: 400;
+    color: ${theme.COLORS.BASE.GRAY_200};
+  `}
+`;
+export const Icon = styled.View`
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 16px;
+`;
