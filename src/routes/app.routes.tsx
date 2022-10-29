7c4e9169c86343a8c13ffe7873382afa12cddf27
genilson <geniilsonfernandes@gmail.com>
2022-10-28 22:54:16 -0300

    feat: config new routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,14 +4,17 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import Statistics from "../screens/Statistics";
 
+export interface AuthStackParamList {
+  Statistics: undefined;
+  Home: undefined;
+  New: undefined;
+}
+
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName="Statistics"
-    >
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
       <Screen name="Home" component={Home} />
       <Screen name="Statistics" component={Statistics} />
     </Navigator>
