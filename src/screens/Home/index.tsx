02504b232fde3eccc465e5b48bacc9c044f65dc3
genilson <geniilsonfernandes@gmail.com>
2022-10-29 01:46:39 -0300

    feat: add section list in home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,69 @@
 import { useNavigation } from "@react-navigation/native";
+import { SectionList, Text, View } from "react-native";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { MealsCard } from "../../components/MealsCard";
 import { PercentCard } from "../../components/PercentCard";
 
 import * as S from "./styles";
 
+const DATA = [
+  {
+    title: "10.10.22",
+    data: [
+      { id: "31", meal: "X-tudo", time: "20:00", diet: false },
+      { id: "1212", meal: "Whey protein com leite", time: "20:00", diet: true }
+    ]
+  },
+  {
+    title: "10.09.22",
+    data: [
+      { id: "131", meal: "X-tudo", time: "20:00", diet: false },
+      { id: "211", meal: "Whey protein com leite", time: "20:00", diet: true },
+      { id: "3211", meal: "Whey protein com leite", time: "20:00", diet: true },
+      {
+        id: "21211",
+        meal: "Whey protein com leite",
+        time: "20:00",
+        diet: true
+      },
+      { id: "2121", meal: "Whey protein com leite", time: "20:00", diet: true },
+      { id: "21311", meal: "Whey protein com leite", time: "20:00", diet: true }
+    ]
+  }
+];
+
 function Home() {
   const navigation = useNavigation();
-
+  const pe = 86;
   const handleShowStatistics = () => {
     navigation.navigate("Statistics", {
-      percent: 86
+      percent: pe
     });
   };
 
   return (
     <S.Wrapper>
       <Header />
-      <PercentCard onPress={() => handleShowStatistics()} percent={45} />
+      <PercentCard onPress={() => handleShowStatistics()} percent={pe} />
       <S.ListHead>
         <S.Label>Refeições</S.Label>
         <Button title="Nova refeição" icon="plus" fill />
       </S.ListHead>
+      <View style={{ flex: 1, paddingBottom: 16 }}>
+        <SectionList
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={{ height: 10 }}></View>}
+          sections={DATA}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <MealsCard title={item.meal} timer={item.time} diet={item.diet} />
+          )}
+          renderSectionHeader={(item) => (
+            <S.SectionTitle>{item.section.title}</S.SectionTitle>
+          )}
+        />
+      </View>
     </S.Wrapper>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,12 +3,14 @@ import styled, { css } from "styled-components/native";
 
 export const Wrapper = styled(SafeAreaView)`
   ${({ theme }) => css`
+    flex: 1;
     padding: 0 ${theme.SPACING.SM}px;
   `}
 `;
 export const ListHead = styled.View`
   ${({ theme }) => css`
-    padding: ${theme.SPACING.XXX}px 0;
+    padding: ${theme.SPACING.XX}px 0;
+    padding-bottom: ${theme.SPACING.SM}px;
   `}
 `;
 export const Label = styled.Text`
@@ -17,5 +19,14 @@ export const Label = styled.Text`
     color: ${theme.COLORS.BASE.GRAY_100};
     font-family: ${theme.FONT_FAMILY.REGULAR};
     margin-bottom: ${theme.SPACING.SM - 4}px;
+  `}
+`;
+export const SectionTitle = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XL}px;
+    color: ${theme.COLORS.BASE.GRAY_100};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    margin-bottom: ${theme.SPACING.SM - 4}px;
+    padding-top: 10px;
   `}
 `;
